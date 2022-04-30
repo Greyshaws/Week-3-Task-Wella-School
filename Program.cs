d0e58e50637e78b1a2405d69058564694fc4d681
Gracious Igwe <gracious0901@gmail.com>
2022-04-30 03:27:31 +0100

    Week3Task

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week3Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number...");
+            int num = int.Parse(Console.ReadLine());
+            int a = 0; // to check if it is a prime number or not
+            for(int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    a++;
+                }
+            }
+            if (a == 2) {
+                Console.WriteLine("It is a prime number", a);
+            } else {
+                Console.WriteLine("Not a prime number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
